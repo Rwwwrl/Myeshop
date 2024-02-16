d2dd242a2d35f38b7f85f82588573aedc022f265
Alexey Sosov <alexey.sosov@zyfra.com>
2024-02-16 09:04:39 +0300

    добавил logging

--- src/eshop/eshop/settings.py
+++ src/eshop/eshop/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import decouple
@@ -136,3 +137,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers":
+        {
+            "root": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+            },
+            "django": {
+                "handlers": ["console"],
+                "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+                "propagate": False,
+            },
+        },
+}
