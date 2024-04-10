28de7dfc0bf23de29e1cb1b4106ca8a90b91b6f9
Alexey Sosov <alexey.sosov@zyfra.com>
2024-04-10 17:30:03 +0300

    [ESHOP-28] feat nginx
    - Добавил nginx, проксирование реализовано черзе unix сокет

--- deploy/uvicorn/uvicorn_run.py
+++ deploy/uvicorn/uvicorn_run.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
     uvicorn.run(
         app='eshop.settings:MAIN_APP',
         reload=False,
-        host='0.0.0.0',
-        port=80,
+        uds='/usr/uvicorn_socket_folder/uvicorn.sock',
         workers=workers,
         access_log='-',
     )
