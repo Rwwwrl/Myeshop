72ae4f652d6519a439d6b7b01ad89d71069d82d0
Alexey Sosov <alexey.sosov@zyfra.com>
2024-05-12 12:42:51 +0300

    [ESHOP-37] fix deploy/uvicorn uvicorn_run.py

--- deploy/uvicorn/uvicorn_run.py
+++ deploy/uvicorn/uvicorn_run.py
@@ -1,13 +1,23 @@
 import multiprocessing
+from pathlib import Path
+from typing import NoReturn
 
 import uvicorn
 
 workers = multiprocessing.cpu_count() * 2 + 1
 
-if __name__ == '__main__':
+UVICORN_SOCKET_PATH = Path('/usr/uvicorn_socket_folder/uvicorn.sock')
+
+
+def main() -> NoReturn:
+    UVICORN_SOCKET_PATH.unlink(missing_ok=True)
     uvicorn.run(
         app='eshop.settings:MAIN_APP',
         reload=False,
-        uds='/usr/uvicorn_socket_folder/uvicorn.sock',
+        uds=UVICORN_SOCKET_PATH,
         workers=workers,
     )
+
+
+if __name__ == '__main__':
+    main()
