96e92776076b79e68790f1da1ac61e7a524dcc2d
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-04 08:35:57 +0300

    [ESHOP-47] feat framework/cqrs cqrs_bus.py
    - Добавил обработку на ошибки на уровне обработчика хендлера

--- eshop/framework/framework/cqrs/cqrs_bus.py
+++ eshop/framework/framework/cqrs/cqrs_bus.py
@@ -13,13 +13,22 @@ class ICQRSBus(abc.ABC):
         raise NotImplementedError
 
 
+class HandlerRaisedAnError(Exception):
+    def __init__(self, *args, original_exception: Exception, **kwargs):
+        self._original_exception_cls_name = original_exception.__class__.__name__
+        self._original_exception_message = str(original_exception)
+
+
 class CQRSBus(ICQRSBus):
     def fetch(self, query: Query) -> QueryResponse:
         from .query.registry import query_registry
 
         handler_cls = query_registry.get_query_handler_cls(query_cls=query.__class__)
         handler = handler_cls()
-        return handler.handle(query=query)
+        try:
+            return handler.handle(query=query)
+        except Exception as e:
+            raise HandlerRaisedAnError(original_exception=e)
 
 
 class CQRSBusSingletoneFactory:
