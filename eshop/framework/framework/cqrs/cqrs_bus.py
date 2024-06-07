ef9d1dc3ceeed84f0316f50c89ddae31b2b52f42
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-07 12:20:50 +0300

    [ESHOP-47] chore framework/cqrs cqrs_bus.py logging

--- eshop/framework/framework/cqrs/cqrs_bus.py
+++ eshop/framework/framework/cqrs/cqrs_bus.py
@@ -1,16 +1,11 @@
 from __future__ import annotations
 
 import abc
+from logging import getLogger
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .query import Query, QueryResponseType
-
-
-class ICQRSBus(abc.ABC):
-    @abc.abstractmethod
-    def fetch(self, query: Query) -> QueryResponseType:
-        raise NotImplementedError
 
 
 class HandlerRaisedAnError(Exception):
@@ -19,16 +14,31 @@ class HandlerRaisedAnError(Exception):
         self._original_exception_message = str(original_exception)
 
 
+class ICQRSBus(abc.ABC):
+    @abc.abstractmethod
+    def fetch(self, query: Query) -> QueryResponseType:
+        raise NotImplementedError
+
+
 class CQRSBus(ICQRSBus):
+
+    _logger = getLogger('CQRSBus')
+
     def fetch(self, query: Query) -> QueryResponseType:
         from .query.registry import query_registry
+
+        self._logger.debug('start processing query %s', query)
 
         handler_cls = query_registry.get_query_handler_cls(query_cls=query.__class__)
         handler = handler_cls()
         try:
-            return handler.handle(query=query)
+            result = handler.handle(query=query)
         except Exception as e:
+            self._logger.debug('query %s was completed with an error', query)
             raise HandlerRaisedAnError(original_exception=e)
+        else:
+            self._logger.debug('query %s was succesfully completed', query)
+            return result
 
 
 class CQRSBusSingletoneFactory:
