e55921e9f3795fb33acd2cee1f348a19f2d6e5f2
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-12 11:59:30 +0300

    [ESHOP-53] feat fixtures поправид создаваемые данные в фикстурах, чтобы они были согласованы

--- eshop/apps/basket/basket/fixtures/create_customer_basket.py
+++ eshop/apps/basket/basket/fixtures/create_customer_basket.py
@@ -11,48 +11,28 @@ def create_customer_basket():
         customer_basket1 = CustomerBasket(buyer_id=1)
         basket_items1 = [
             BasketItem(
+                id=1,
                 basket=customer_basket1,
                 product_id=1,
-                product_name='product_name1',
+                product_name='name1',
                 unit_price=10,
                 quantity=1,
-                picture_url='picture_url1',
+                picture_url='root/filename1',
             ),
             BasketItem(
+                id=2,
                 basket=customer_basket1,
                 product_id=2,
-                product_name='product_name2',
-                unit_price=10,
+                product_name='name2',
+                unit_price=20,
                 quantity=2,
-                picture_url='picture_url2',
-            ),
-        ]
-
-        customer_basket2 = CustomerBasket(buyer_id=2)
-        basket_items2 = [
-            BasketItem(
-                basket=customer_basket2,
-                product_id=1,
-                product_name='product_name1',
-                unit_price=10,
-                quantity=1,
-                picture_url='picture_url1',
-            ),
-            BasketItem(
-                basket=customer_basket2,
-                product_id=2,
-                product_name='product_name2',
-                unit_price=10,
-                quantity=2,
-                picture_url='picture_url2',
+                picture_url='root/filename2',
             ),
         ]
 
         session.add_all([
             customer_basket1,
-            customer_basket2,
             *basket_items1,
-            *basket_items2,
         ])
 
         session.commit()
