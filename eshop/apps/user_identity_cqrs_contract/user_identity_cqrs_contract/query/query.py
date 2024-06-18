4e503da4cadb18efef6551a559c7b7c219fadb92
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-18 16:16:45 +0300

    [ESHOP-52] feat user_identity_cqrs_contract InvalidJwtTokenError добавил возможную ошибку для UserIdFromJWTTokenQuery

--- eshop/apps/user_identity_cqrs_contract/user_identity_cqrs_contract/query/query.py
+++ eshop/apps/user_identity_cqrs_contract/user_identity_cqrs_contract/query/query.py
@@ -1,5 +1,8 @@
+from typing import Final
+
 from attrs import define
 
+from framework.cqrs.exceptions import PossibleExpectedError
 from framework.cqrs.query.query import Query
 
 from user_identity_cqrs_contract.hints import JWTToken
@@ -9,7 +12,13 @@ from .query_response import UserDTO
 __all__ = ('UserIdFromJWTTokenQuery', )
 
 
+class InvalidJwtTokenError(PossibleExpectedError):
+    pass
+
+
 @define
 class UserIdFromJWTTokenQuery(Query[UserDTO]):
 
     jwt_token: JWTToken
+
+    __possible_exceptions__: Final = (InvalidJwtTokenError, )
