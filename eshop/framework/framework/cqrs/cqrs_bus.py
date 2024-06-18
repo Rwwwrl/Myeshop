7d4dfe7fbfc22c2c1d5f760c433bf7afd379b50b
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-18 13:33:26 +0300

    [ESHOP-52] feat framework/cqrs cqrs_bus.py CQRSBus.execute реализовал запуск команды

--- eshop/framework/framework/cqrs/cqrs_bus.py
+++ eshop/framework/framework/cqrs/cqrs_bus.py
@@ -2,15 +2,16 @@ from __future__ import annotations
 
 import abc
 from logging import getLogger
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 from . import registry
 
 if TYPE_CHECKING:
-    from .query import Query, QueryResponseType
+    from .query import Query, QueryResponseType, IQueryHandler
+    from .command import Command, CommandResponseType, ICommandHandler
 
 
-class HandlerRaisedAnError(Exception):
+class CQRSException(Exception):
     def __init__(self, *args, original_exception: Exception, **kwargs):
         self._original_exception_cls_name = original_exception.__class__.__name__
         self._original_exception_message = str(original_exception)
@@ -29,14 +30,26 @@ class CQRSBus(ICQRSBus):
     def fetch(self, query: Query) -> QueryResponseType:
         self._logger.debug('start processing query %s', query)
 
-        handler_cls = registry.get_registry().get_handler_cls(request_cls=type(query))
+        handler_cls = cast(IQueryHandler, registry.get_registry().get_handler_cls(request_cls=type(query)))
         try:
             result = handler_cls().handle(query=query)
         except Exception as e:
             self._logger.debug('query %s was completed with an error', query)
-            raise HandlerRaisedAnError(original_exception=e)
+            raise CQRSException(original_exception=e)
         else:
             self._logger.debug('query %s was succesfully completed', query)
+            return result
+
+    def execute(self, command: Command) -> CommandResponseType:
+        self._logger.debug('start processing command %s', command)
+        handler_cls = cast(ICommandHandler, registry.get_registry().get_handler_cls(request_cls=type(command)))
+        try:
+            result = handler_cls().handle(command=command)
+        except Exception as e:
+            self._logger.debug('command %s was completed with an error', command)
+            raise CQRSException(original_exception=e)
+        else:
+            self._logger.debug('command %s was succesfully completed', command)
             return result
 
 
--- eshop/framework/framework/fastapi/dependencies/get_user_from_request.py
+++ eshop/framework/framework/fastapi/dependencies/get_user_from_request.py
@@ -4,7 +4,7 @@ from fastapi import Depends, HTTPException, status
 
 from eshop.settings import settings
 
-from framework.cqrs.cqrs_bus import HandlerRaisedAnError
+from framework.cqrs.cqrs_bus import CQRSException
 
 from user_identity_cqrs_contract import hints
 from user_identity_cqrs_contract.query import UserIdFromJWTTokenQuery
@@ -13,7 +13,7 @@ from user_identity_cqrs_contract.query import UserIdFromJWTTokenQuery
 def get_user_from_http_request(jwt_token: Annotated[hints.JWTToken, Depends(settings.OAUTH2_SCHEME)]) -> hints.UserId:
     try:
         return UserIdFromJWTTokenQuery(jwt_token=jwt_token).fetch().id
-    except HandlerRaisedAnError:
+    except CQRSException:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='invalid jwt token')
 
     except Exception:
