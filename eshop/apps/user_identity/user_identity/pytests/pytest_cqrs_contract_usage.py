ae526d8c0a56b395d673328f41a1ef63a78d4679
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-18 10:33:45 +0300

    [ESHOP-52] chore tests поправил формат старых тестов

--- eshop/apps/user_identity/user_identity/pytests/pytest_cqrs_contract_usage.py
+++ eshop/apps/user_identity/user_identity/pytests/pytest_cqrs_contract_usage.py
@@ -4,6 +4,8 @@ import pytest
 
 from pytest_check import check
 
+from framework.for_pytests.for_testing_cqrs_contract_usage import ITestQueryContract
+
 from user_identity_cqrs_contract.hints import JWTToken
 from user_identity_cqrs_contract.query.query import UserIdFromJWTTokenQuery
 from user_identity_cqrs_contract.query.query_response import (
@@ -13,16 +15,16 @@ from user_identity_cqrs_contract.query.query_response import (
 
 
 @pytest.mark.cqrs_contract_usage
-class TestUserIdFromJWTTokenQuery:
-    def test_query(self):
+class TestUserIdFromJWTTokenQuery(ITestQueryContract[UserIdFromJWTTokenQuery]):
+    def test_query_contract(self):
         with check:
             assert hasattr(attrs.fields(UserIdFromJWTTokenQuery), 'jwt_token')
             assert attrs.fields(UserIdFromJWTTokenQuery).jwt_token.type == JWTToken
 
-    def test_response(self):
+    def test_query_response_contract(self):
         response_type = UserIdFromJWTTokenQuery.__response_type__()
 
         with check:
             assert response_type == UserDTO
-            assert UserDTO.model_fields.get('id', None)
+            assert UserDTO.model_fields.get('id', None) is not None
             assert UserDTO.model_fields['id'].annotation == UserId
