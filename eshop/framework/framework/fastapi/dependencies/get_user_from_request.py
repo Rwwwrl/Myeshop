53dca3c10c78af84f8f6423169c8e645d5b3999d
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-18 16:15:59 +0300

    [ESHOP-52] chore framework/fastapi/dependecies get_user_from_request.py

--- eshop/framework/framework/fastapi/dependencies/get_user_from_request.py
+++ eshop/framework/framework/fastapi/dependencies/get_user_from_request.py
@@ -4,17 +4,17 @@ from fastapi import Depends, HTTPException, status
 
 from eshop.settings import settings
 
-from framework.cqrs.cqrs_bus import CQRSException
+from framework.cqrs.exceptions import UnexpectedError
 
 from user_identity_cqrs_contract import hints
 from user_identity_cqrs_contract.query import UserIdFromJWTTokenQuery
+from user_identity_cqrs_contract.query.query import InvalidJwtTokenError
 
 
 def get_user_from_http_request(jwt_token: Annotated[hints.JWTToken, Depends(settings.OAUTH2_SCHEME)]) -> hints.UserId:
     try:
         return UserIdFromJWTTokenQuery(jwt_token=jwt_token).fetch().id
-    except CQRSException:
+    except InvalidJwtTokenError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='invalid jwt token')
-
-    except Exception:
+    except UnexpectedError:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
