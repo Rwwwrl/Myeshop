9c5620ce32a23d538ce7bddadcee0f9eba841f09
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-21 21:38:40 +0300

    [ESHOP-52] tests pytest_cqrs_contract_usage добавил assert_attribute в старые тесты

--- eshop/apps/user_identity/user_identity/pytests/pytest_cqrs_contract_usage.py
+++ eshop/apps/user_identity/user_identity/pytests/pytest_cqrs_contract_usage.py
@@ -1,10 +1,8 @@
-import attrs
-
 import pytest
 
 from pytest_check import check
 
-from framework.for_pytests.for_testing_cqrs_contract_usage import ITestQueryContract
+from framework.for_pytests.for_testing_cqrs_contract_usage import ITestQueryContract, assert_attribute
 
 from user_identity_cqrs_contract.hints import JWTToken
 from user_identity_cqrs_contract.query.query import UserIdFromJWTTokenQuery
@@ -18,13 +16,11 @@ from user_identity_cqrs_contract.query.query_response import (
 class TestUserIdFromJWTTokenQuery(ITestQueryContract[UserIdFromJWTTokenQuery]):
     def test_query_contract(self):
         with check:
-            assert hasattr(attrs.fields(UserIdFromJWTTokenQuery), 'jwt_token')
-            assert attrs.fields(UserIdFromJWTTokenQuery).jwt_token.type == JWTToken
+            assert_attribute(UserIdFromJWTTokenQuery, 'jwt_token', JWTToken)
 
     def test_query_response_contract(self):
         response_type = UserIdFromJWTTokenQuery.__response_type__()
 
         with check:
             assert response_type == UserDTO
-            assert UserDTO.model_fields.get('id', None) is not None
-            assert UserDTO.model_fields['id'].annotation == UserId
+            assert_attribute(UserDTO, 'id', UserId)
