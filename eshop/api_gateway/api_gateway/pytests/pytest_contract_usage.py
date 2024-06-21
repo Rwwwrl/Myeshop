5031d5191fa940eae0901243096ba1819d78523a
Alexey Sosov <alexey.sosov@zyfra.com>
2024-06-21 21:50:18 +0300

    [ESHOP-52] tests api_gateway/pytest_contract_usage.py TestUpdateCustomerBasketCommand
    - забыл написать тест на команду при тестировании update_basket

--- eshop/api_gateway/api_gateway/pytests/pytest_contract_usage.py
+++ eshop/api_gateway/api_gateway/pytests/pytest_contract_usage.py
@@ -1,39 +1,78 @@
 from typing import List
 
-import attrs
-
 import pytest
 
 from pytest_check import check
+
+import basket_cqrs_contract.command.command
+import basket_cqrs_contract.hints
+from basket_cqrs_contract.command import UpdateCustomerBasketCommand
+from basket_cqrs_contract.query import CustomerBasketQuery
+from basket_cqrs_contract.query.query_response import BasketItemDTO, CustomerBasketDTO
 
 import catalog_cqrs_contract.hints
 from catalog_cqrs_contract.query import CatalogItemsByIdsQuery
 from catalog_cqrs_contract.query.query_response import CatalogItemDTO
 
-from framework.for_pytests.for_testing_cqrs_contract_usage import ITestQueryContract
+from framework.for_pytests.for_testing_cqrs_contract_usage import (
+    ITestCommandContract,
+    ITestQueryContract,
+    assert_attribute,
+)
 
 
 @pytest.mark.cqrs_contract_usage
 class TestCatalogItemByIdsQuery(ITestQueryContract[CatalogItemsByIdsQuery]):
     def test_query_contract(self):
         with check:
-            assert hasattr(attrs.fields(CatalogItemsByIdsQuery), 'ids')
-            assert attrs.fields(CatalogItemsByIdsQuery).ids.type == List[catalog_cqrs_contract.hints.CatalogItemId]
+            assert_attribute(CatalogItemsByIdsQuery, 'ids', List[catalog_cqrs_contract.hints.CatalogItemId])
 
     def test_query_response_contract(self):
         response_type = CatalogItemsByIdsQuery.__response_type__()
 
         with check:
             assert response_type == List[CatalogItemDTO]
+            assert_attribute(CatalogItemDTO, 'id', catalog_cqrs_contract.hints.CatalogItemId)
+            assert_attribute(CatalogItemDTO, 'name', str)
+            assert_attribute(CatalogItemDTO, 'price', float)
+            assert_attribute(CatalogItemDTO, 'picture_url', str)
 
-            assert CatalogItemDTO.model_fields.get('id', None) is not None
-            assert CatalogItemDTO.model_fields['id'].annotation == catalog_cqrs_contract.hints.CatalogItemId
 
-            assert CatalogItemDTO.model_fields.get('name', None) is not None
-            assert CatalogItemDTO.model_fields['name'].annotation == str
+class TestUpdateCustomerBasketCommand(ITestCommandContract[UpdateCustomerBasketCommand]):
+    def test_command_contract(self) -> None:
+        with check:
+            assert_attribute(
+                UpdateCustomerBasketCommand,
+                'buyer_id',
+                basket_cqrs_contract.hints.CustomerId,
+            )
+            assert_attribute(
+                UpdateCustomerBasketCommand,
+                'basket_items',
+                List[basket_cqrs_contract.command.command.BasketItemDTO],
+            )
+            assert_attribute(
+                basket_cqrs_contract.command.command.BasketItemDTO,
+                'product_id',
+                basket_cqrs_contract.hints.ProductId,
+            )
+            assert_attribute(
+                basket_cqrs_contract.command.command.BasketItemDTO,
+                'product_name',
+                basket_cqrs_contract.hints.ProductName,
+            )
+            assert_attribute(
+                basket_cqrs_contract.command.command.BasketItemDTO,
+                'quantity',
+                basket_cqrs_contract.hints.Quantity,
+            )
+            assert_attribute(
+                basket_cqrs_contract.command.command.BasketItemDTO,
+                'picture_url',
+                basket_cqrs_contract.hints.PictureUrl,
+            )
 
-            assert CatalogItemDTO.model_fields.get('price', None) is not None
-            assert CatalogItemDTO.model_fields['price'].annotation == float
+    def test_command_response_contract(self) -> None:
+        # мы не используем ответ от команды
+        pass
 
-            assert CatalogItemDTO.model_fields.get('picture_url', None) is not None
-            assert CatalogItemDTO.model_fields['picture_url'].annotation == str
