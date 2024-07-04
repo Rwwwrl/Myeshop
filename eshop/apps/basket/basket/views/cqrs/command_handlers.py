Rwwwrl/my_eshoponcontainer

[ESHOP-68] feat basket command_handlers.py

eshop/apps/basket/basket/views/cqrs/command_handlers.py
@@ -1,5 +1,11 @@
-from basket.domain.models.basket_item import BasketItem
-from basket.domain.models.customer_basket import CustomerBasket, CustomerBasketRepository
+from basket.infrastructure.persistence.postgres.customer_basket import (
+    CustomerBasketORM,
+    PostgresCustomerBasketRepository,
+)
+from basket.infrastructure.persistence.postgres.customer_basket.customer_basket_orm import (
+    BasketItem,
+    Data,
+)
 
 from basket_cqrs_contract.command import UpdateCustomerBasketCommand
 
@@ -10,23 +16,26 @@ from framework.sqlalchemy.session_factory import session_factory
 @UpdateCustomerBasketCommand.handler
 class UpdateCustomerBasketCommandHandler(ICommandHandler):
     @staticmethod
-    def _deserialize_to_orm(command: UpdateCustomerBasketCommand) -> CustomerBasket:
-        return CustomerBasket(
-            buyer_id=command.buyer_id,
-            basket_items=[
-                BasketItem(
-                    product_id=basket_item.product_id,
-                    product_name=basket_item.product_name,
-                    unit_price=basket_item.unit_price,
-                    quantity=basket_item.quantity,
-                    picture_url=basket_item.picture_url,
-                ) for basket_item in command.basket_items
-            ],
+    def _deserialize_to_orm(command: UpdateCustomerBasketCommand) -> CustomerBasketORM:
+        return CustomerBasketORM(
+            buyer_id=command.customer_basket.buyer_id,
+            data=Data(
+                basket_items=[
+                    BasketItem(
+                        id=basket_item.id,
+                        product_id=basket_item.product_id,
+                        product_name=basket_item.product_name,
+                        unit_price=basket_item.unit_price,
+                        quantity=basket_item.quantity,
+                        picture_url=basket_item.quantity,
+                    ) for basket_item in command.customer_basket.basket_items
+                ],
+            ),
         )
 
     def handle(self, command: UpdateCustomerBasketCommand) -> None:
-        customer_basket = self._deserialize_to_orm(command)
+        customer_basket = self._deserialize_to_orm(command=command)
         with session_factory() as session:
-            customer_basket_repository = CustomerBasketRepository(session=session)
-            customer_basket_repository.update(customer_basket=customer_basket)
+            customer_basket_repository = PostgresCustomerBasketRepository(session=session)
+            customer_basket_repository.save(customer_basket_orm=customer_basket)
             session.commit()

Rwwwrl/Myeshop

eshop/apps/basket/basket/views/cqrs/command_handlers.py
@@ -1,5 +1,11 @@
-from basket.domain.models.basket_item import BasketItem
-from basket.domain.models.customer_basket import CustomerBasket, CustomerBasketRepository
+from basket.infrastructure.persistence.postgres.customer_basket import (
+    CustomerBasketORM,
+    PostgresCustomerBasketRepository,
+)
+from basket.infrastructure.persistence.postgres.customer_basket.customer_basket_orm import (
+    BasketItem,
+    Data,
+)
 
 from basket_cqrs_contract.command import UpdateCustomerBasketCommand
 
@@ -10,23 +16,26 @@ from framework.sqlalchemy.session_factory import session_factory
 @UpdateCustomerBasketCommand.handler
 class UpdateCustomerBasketCommandHandler(ICommandHandler):
     @staticmethod
-    def _deserialize_to_orm(command: UpdateCustomerBasketCommand) -> CustomerBasket:
-        return CustomerBasket(
-            buyer_id=command.buyer_id,
-            basket_items=[
-                BasketItem(
-                    product_id=basket_item.product_id,
-                    product_name=basket_item.product_name,
-                    unit_price=basket_item.unit_price,
-                    quantity=basket_item.quantity,
-                    picture_url=basket_item.picture_url,
-                ) for basket_item in command.basket_items
-            ],
+    def _deserialize_to_orm(command: UpdateCustomerBasketCommand) -> CustomerBasketORM:
+        return CustomerBasketORM(
+            buyer_id=command.customer_basket.buyer_id,
+            data=Data(
+                basket_items=[
+                    BasketItem(
+                        id=basket_item.id,
+                        product_id=basket_item.product_id,
+                        product_name=basket_item.product_name,
+                        unit_price=basket_item.unit_price,
+                        quantity=basket_item.quantity,
+                        picture_url=basket_item.picture_url,
+                    ) for basket_item in command.customer_basket.basket_items
+                ],
+            ),
         )
 
     def handle(self, command: UpdateCustomerBasketCommand) -> None:
-        customer_basket = self._deserialize_to_orm(command)
+        customer_basket_orm = self._deserialize_to_orm(command=command)
         with session_factory() as session:
-            customer_basket_repository = CustomerBasketRepository(session=session)
-            customer_basket_repository.update(customer_basket=customer_basket)
+            customer_basket_repository = PostgresCustomerBasketRepository(session=session)
+            customer_basket_repository.save(customer_basket_orm=customer_basket_orm)
             session.commit()