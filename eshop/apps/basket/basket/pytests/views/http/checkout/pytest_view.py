54afbbe474d0174138175ee438ecf8baaaf03f29
Rwwwrl <forgithublesh@yandex.ru>
2024-07-08 16:06:23 +0300

    [ESHOP-69] feat basket/views/http checkout
    - Реализовал эвент UserCheckoutAcceptedEvent (пока что без обработчика)
    - реализовал http эндпоинт checkout
    - реализовал тесты на checkout

---
+++ eshop/apps/basket/basket/pytests/views/http/checkout/pytest_view.py
@@ -0,0 +1,156 @@
+from datetime import date
+
+from fastapi import status
+
+from mock import Mock, patch
+
+import pytest
+
+from typing_extensions import TypedDict
+
+from basket.app_config import BasketAppConfig
+from basket.views.http.checkout import checkout
+from basket.views.http.checkout.dto import BasketCheckoutRequestData
+
+from basket_cqrs_contract.customer_basket_dto import BasketItemDTO, CustomerBasketDTO
+from basket_cqrs_contract.event import UserCheckoutAcceptedEvent
+from basket_cqrs_contract.query import CustomerBasketQuery
+
+from framework.for_pytests.for_testing_http_views import ExpectedHttpResponse
+from framework.for_pytests.test_case import TestCase as _TestCase
+from framework.for_pytests.test_class import TestClass
+
+from user_identity_cqrs_contract.hints import UserId
+from user_identity_cqrs_contract.query import UserQuery
+from user_identity_cqrs_contract.query.query_response import UserDTO
+
+
+class UserCheckoutAcceptedEventExpectedCallArgs(TypedDict):
+
+    user_id: int
+    username: str
+    order_number: int
+    city: str
+    street: str
+    state: str
+    country: str
+    zip_code: str
+    card_number: str
+    card_holder_name: str
+    card_expiration: date
+    card_security_number: str
+    card_type_id: int
+    basket: CustomerBasketDTO
+
+
+class TestCase(_TestCase['TestCheckoutView']):
+    request_data: BasketCheckoutRequestData
+    user_id: UserId
+
+    mock__user_query__fetch__return_value: UserDTO
+    mock__customer_basket_query__fetch__return_value: CustomerBasketDTO
+
+    mock__user_checkout_accepted_event__expected_call_args: UserCheckoutAcceptedEventExpectedCallArgs
+
+    expected_http_response: ExpectedHttpResponse
+
+
+@pytest.fixture(scope='session')
+def test_case() -> TestCase:
+    request_data = BasketCheckoutRequestData(
+        order_number=1,
+        city='city',
+        street='street',
+        state='state',
+        country='country',
+        zip_code='zip_code',
+        card_number='card_number',
+        card_holder_name='card_holder_name',
+        card_expiration=date(year=2000, month=1, day=2),
+        card_security_number='card_security_number',
+        card_type_id=1,
+    )
+
+    user_id = 1
+
+    mock__customer_basket_query__fetch = CustomerBasketDTO(
+        buyer_id=user_id,
+        basket_items=[
+            BasketItemDTO(
+                id=1,
+                product_id=1,
+                product_name='product_name1',
+                unit_price=10,
+                quantity=20,
+                picture_url='picture_url1',
+            ),
+            BasketItemDTO(
+                id=2,
+                product_id=2,
+                product_name='product_name2',
+                unit_price=10,
+                quantity=20,
+                picture_url='picture_url2',
+            ),
+        ],
+    )
+
+    mock__user_query__fetch = UserDTO(
+        id=user_id,
+        name='name',
+    )
+
+    mock__user_checkout_accepted_event__expected_call_args: UserCheckoutAcceptedEventExpectedCallArgs = {
+        **request_data.model_dump(mode='python'),
+        'basket': mock__customer_basket_query__fetch,
+        'user_id': user_id,
+        'username': mock__user_query__fetch.name,
+    }
+
+    return TestCase(
+        request_data=request_data,
+        user_id=user_id,
+        mock__customer_basket_query__fetch__return_value=mock__customer_basket_query__fetch,
+        mock__user_query__fetch__return_value=mock__user_query__fetch,
+        mock__user_checkout_accepted_event__expected_call_args=mock__user_checkout_accepted_event__expected_call_args,
+        expected_http_response=ExpectedHttpResponse(
+            status_code=status.HTTP_200_OK,
+            body=b'',
+        ),
+    )
+
+
+class TestUrlToView(TestClass[checkout]):
+    def test(self):
+        expected_url = '/basket/checkout/'
+        fact_url = BasketAppConfig.get_api_router().url_path_for(checkout.__name__)
+        assert fact_url == expected_url
+
+
+class TestCheckoutView(TestClass[checkout]):
+    @patch.object(UserQuery, 'fetch')
+    @patch.object(CustomerBasketQuery, 'fetch')
+    @patch.object(UserCheckoutAcceptedEvent, '__init__')
+    @patch.object(UserCheckoutAcceptedEvent, 'publish')
+    def test(
+        self,
+        mock__user_checkout_accepted_event__publish: Mock,
+        mock__user_checkout_accepted_event__init: Mock,
+        mock__customer_basket_query__fetch: Mock,
+        mock__user_query__fetch: Mock,
+        test_case: TestCase,
+    ):
+
+        mock__user_query__fetch.return_value = test_case.mock__user_query__fetch__return_value
+        mock__customer_basket_query__fetch.return_value = test_case.mock__customer_basket_query__fetch__return_value
+
+        mock__user_checkout_accepted_event__init.return_value = None
+        mock__user_checkout_accepted_event__publish.return_value = None
+
+        response = checkout(request_data=test_case.request_data, user_id=test_case.user_id)
+        assert response.status_code == test_case.expected_http_response.status_code
+        assert response.body == test_case.expected_http_response.body
+
+        mock__user_checkout_accepted_event__init.assert_called_once_with(
+            **test_case.mock__user_checkout_accepted_event__expected_call_args,
+        )
---
+++ eshop/apps/basket_cqrs_contract/basket_cqrs_contract/event.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from pydantic.types import PositiveInt
+
+from framework.cqrs.event import Event
+
+from .customer_basket_dto import CustomerBasketDTO
+
+
+class UserCheckoutAcceptedEvent(Event):
+
+    user_id: PositiveInt
+    username: str
+    order_number: PositiveInt
+    city: str
+    street: str
+    state: str
+    country: str
+    zip_code: str
+    card_number: str
+    card_holder_name: str
+    card_expiration: datetime
+    card_security_number: str
+    card_type_id: PositiveInt
+    basket: CustomerBasketDTO
