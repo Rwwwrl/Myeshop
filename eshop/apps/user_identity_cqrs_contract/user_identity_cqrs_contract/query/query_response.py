b57aee3490a345d17347f39a655d2b38fb2eb87b
Rwwwrl <forgithublesh@yandex.ru>
2024-07-26 16:38:14 +0300

    [ESHOP-83] feat catalog_cqrs_contract в ответ UserFromJWTTokenQuery вместе с id теперь возвращаем еще и role

--- eshop/apps/user_identity_cqrs_contract/user_identity_cqrs_contract/query/query_response.py
+++ eshop/apps/user_identity_cqrs_contract/user_identity_cqrs_contract/query/query_response.py
@@ -1,6 +1,18 @@
+from enum import Enum
+
 from framework.common.dto import DTO
 
 from user_identity_cqrs_contract import hints
+
+
+class UserRoleEnum(Enum):
+    ADMIN = 'ADMIN'
+    CUSTOMER = 'CUSTOMER'
+
+
+class UserIdWithRoleDTO(DTO):
+    id: hints.UserId
+    role: UserRoleEnum
 
 
 class UserDTO(DTO):
