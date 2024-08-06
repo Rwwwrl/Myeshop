373ccaeb04abf9103def6d51f1194138c950f70e
Rwwwrl <forgithublesh@yandex.ru>
2024-08-06 09:11:40 +0300

    [ESHOP-82] feat test_app views test_file_upload

--- eshop/apps/test_app/test_app/views.py
+++ eshop/apps/test_app/test_app/views.py
@@ -1,12 +1,15 @@
-from fastapi import Response, UploadFile as fastapi_UploadFile, status
+from typing import Annotated
+
+from fastapi import Depends, Response, UploadFile as fastapi_UploadFile, status
 
 from sqlalchemy import select
 
 from test_app import hints
 
+from eshop.dependency_container import dependency_container
+
 from framework.common.dto import DTO
 from framework.file_storage import UploadFile
-from framework.file_storage.local_file_storage.local_file_storage_maker import LocalFileStorage
 from framework.sqlalchemy.session import Session
 
 from .api_router import api_router
@@ -94,17 +97,23 @@ def test() -> dict:
 
 @api_router.post('/file_upload/')
 def test_file_upload(upload_file: fastapi_UploadFile) -> Response:
-    LocalFileStorage().upload(
-        upload_file=UploadFile(file=upload_file.file, filename=upload_file.filename),
-        space='test_app',
+    url_path_to_file = dependency_container.file_storage_api().upload(
+        upload_file=UploadFile(
+            file=upload_file.file,
+            filename=upload_file.filename,
+        ),
     )
-    return Response(status_code=status.HTTP_200_OK)
+    return url_path_to_file
 
 
 class TestRequestBodyWithFileBody(DTO):
-    message: str
+    field1: int
+    field2: int
 
 
 @api_router.post('/test_request_body_with_file/')
-def test_request_body_with_file(upload_file: fastapi_UploadFile, body: TestRequestBodyWithFileBody) -> Response:
+def test_request_body_with_file(
+    upload_file: fastapi_UploadFile,
+    body: Annotated[TestRequestBodyWithFileBody, Depends()],
+) -> Response:
     return Response(status_code=status.HTTP_200_OK)
