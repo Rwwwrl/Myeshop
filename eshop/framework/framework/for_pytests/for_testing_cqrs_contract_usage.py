4f3e4b5a336749d080ab9960f2edf6630ddcfa22
Rwwwrl <forgithublesh@yandex.ru>
2024-08-15 21:53:56 +0300

    [ESHOP-110] feat framework for_pytests/for_testing_cqrs_contract_usage.py _get_fact_type_from_complex_type

--- eshop/framework/framework/for_pytests/for_testing_cqrs_contract_usage.py
+++ eshop/framework/framework/for_pytests/for_testing_cqrs_contract_usage.py
@@ -1,4 +1,7 @@
 import abc
+import types
+import typing as t
+from datetime import datetime, timedelta
 from typing import Any, Type, TypeVar, Union
 
 import attrs
@@ -7,7 +10,9 @@ from pydantic import BaseModel
 
 from pytest_check import check
 
+from framework.common.dto import DTO
 from framework.cqrs.command import ISyncCommand
+from framework.cqrs.context import IContext
 from framework.cqrs.event import IEvent
 from framework.cqrs.query import IQuery
 from framework.for_pytests.test_class import TestClass
@@ -67,6 +72,93 @@ class ITestEventContract(abc.ABC, TestClass[EventTypeVar]):
         raise NotImplementedError
 
 
+class ThisComplexTypeIsNotYetSupported:
+    pass
+
+
+def _get_fact_type_from_complex_type(complex_type: Any) -> Any:
+    """
+    функция получения 'fact_type' из 'complex_type'
+
+    посмотреть примеры можно в
+        framework.pytests.for_pytests.pytest_for_testing_cqrs_contract_usage.TestGetFactTypeFromComplexType
+    """
+
+    fact_types = set([int, float, str, set, list, dict, tuple, datetime, timedelta])
+    if complex_type in fact_types:
+        return complex_type
+
+    if isinstance(complex_type, t._AnnotatedAlias):
+        # к этому относится Annotated
+        return _get_fact_type_from_complex_type(complex_type.__origin__)
+
+    if isinstance(complex_type, t._UnionGenericAlias):
+        if complex_type._name == 'Union':
+            return t.Union[*[_get_fact_type_from_complex_type(arg) for arg in complex_type.__args__]]    # type: ignore
+
+        if complex_type._name == 'Optional':
+            return t.Optional[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+        # set | int является инстансом types.UnionType, но
+        # t.NewType('name', str) | t.NewType('name', int) ялвяется инстансом typing.Union
+        # при этом _name = None, но _origin указывает на typing.Union
+        if complex_type.__origin__ is t.Union:
+            # TODO: пока что не придумал как возвращать в таком же ввиде через "|",
+            # пока что возвращаю через typing.Union
+            return t.Union[*[_get_fact_type_from_complex_type(arg) for arg in complex_type.__args__]]    # type: ignore
+
+    if isinstance(complex_type, t.NewType):
+        return _get_fact_type_from_complex_type(complex_type.__supertype__)
+
+    if isinstance(complex_type, types.UnionType):
+        # относится к такой записи "int | str"
+
+        # TODO: пока что не придумал как возвращать в таком же ввиде через "|",
+        # пока что возвращаю через typing.Union
+        return t.Union[*[_get_fact_type_from_complex_type(arg) for arg in complex_type.__args__]]    # type: ignore
+
+    if isinstance(complex_type, types.GenericAlias):
+        if complex_type.__origin__ is list:
+            return list[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+        if complex_type.__origin__ is dict:
+            return dict[
+                _get_fact_type_from_complex_type(complex_type.__args__[0]),
+                _get_fact_type_from_complex_type(complex_type.__args__[1]),
+            ]
+
+        if complex_type.__origin__ is set:
+            return set[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+        if complex_type.__origin__ is tuple:
+            return tuple[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+    if isinstance(complex_type, t._GenericAlias):
+        # к t._GenericAlias относятся все typing.List, typing.Dict, typing.Set
+        if complex_type.__origin__ is list:
+            return t.List[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+        if complex_type.__origin__ is dict:
+            return t.Dict[
+                _get_fact_type_from_complex_type(complex_type.__args__[0]),
+                _get_fact_type_from_complex_type(complex_type.__args__[1]),
+            ]
+
+        if complex_type.__origin__ is set:
+            return t.Set[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+        if complex_type.__origin__ is tuple:
+            return t.Tuple[_get_fact_type_from_complex_type(complex_type.__args__[0])]
+
+    if issubclass(complex_type, IContext):
+        return complex_type
+
+    if issubclass(complex_type, DTO):
+        return complex_type
+
+    raise ThisComplexTypeIsNotYetSupported(f'complex_type = {complex_type}')
+
+
 class AttributeValidator:
     @staticmethod
     def _is_pydantic_cls(validating_cls: Type) -> bool:
@@ -80,36 +172,45 @@ class AttributeValidator:
     def _assert_attribute__attrs(
         validating_cls: Type[attrs.AttrsInstance],
         attribute_name: str,
-        expected_type: Any,
+        expected_fact_type: Any,
     ) -> None:
         with check:
             assert hasattr(attrs.fields(validating_cls), attribute_name)
-            assert getattr(attrs.fields(validating_cls), attribute_name).type == expected_type
+
+            fact_type = _get_fact_type_from_complex_type(getattr(attrs.fields(validating_cls), attribute_name).type)
+            assert fact_type == expected_fact_type
 
     @staticmethod
     def _assert_attribute__pydantic(
         validating_cls: Type[BaseModel],
         attribute_name: str,
-        expected_type: Any,
+        expected_fact_type: Any,
     ) -> None:
         with check:
             assert validating_cls.model_fields.get(attribute_name, None) is not None
-            assert validating_cls.model_fields[attribute_name].annotation == expected_type
+
+            fact_type = _get_fact_type_from_complex_type(validating_cls.model_fields[attribute_name].annotation)
+            assert fact_type == expected_fact_type
 
     def __call__(
         self,
         validating_cls: Union[Type[attrs.AttrsInstance], Type[BaseModel]],
         attribute_name: str,
-        expected_type: Any,
+        expected_fact_type: Any,
     ) -> None:
         """
-        Проверяем, что у `validating_cls` есть атрибут с именем `attribute_name` и его тип равен `expected_type`
+        Проверяем, что у `validating_cls` есть атрибут с именем `attribute_name`
+        и его 'фактический тип'* равен `expected_type`
+
+        что значит "фактический тип" смотреть тут
+        https://www.notion.so/rwwwrl/cqrs-6d2febc0faa541a6906ba22e6ef2f2e0?pvs=4
+
         """
         if self._is_pydantic_cls(validating_cls):
             self._assert_attribute__pydantic(
                 validating_cls=validating_cls,
                 attribute_name=attribute_name,
-                expected_type=expected_type,
+                expected_fact_type=expected_fact_type,
             )
             return
 
@@ -117,7 +218,7 @@ class AttributeValidator:
             self._assert_attribute__attrs(
                 validating_cls=validating_cls,
                 attribute_name=attribute_name,
-                expected_type=expected_type,
+                expected_fact_type=expected_fact_type,
             )
             return
 
---
+++ eshop/framework/framework/pytests/for_pytests/pytest_for_testing_cqrs_contract_usage.py
@@ -0,0 +1,234 @@
+import typing as t
+from datetime import datetime, timedelta
+
+import pytest
+
+import pytest_lazyfixture
+
+from framework.common.dto import DTO
+from framework.cqrs.context import IContext
+from framework.for_pytests.for_testing_cqrs_contract_usage import _get_fact_type_from_complex_type
+from framework.for_pytests.test_case import TestCase as _TestCase
+from framework.for_pytests.test_class import TestClass
+
+
+class TestCase(_TestCase['TestGetFactTypeFromComplexType']):
+    complex_type: t.Any
+    expected_fact_type: t.Any
+
+
+@pytest.fixture(scope='session', params=[int, float, str, set, list, dict, tuple, datetime, timedelta])
+def test_case_fact_types(request) -> TestCase:
+    return TestCase(complex_type=request.param, expected_fact_type=request.param)
+
+
+@pytest.fixture(scope='session')
+def test_case_float() -> TestCase:
+    return TestCase(complex_type=float, expected_fact_type=float)
+
+
+@pytest.fixture(scope='session')
+def test_case_NewType() -> TestCase:
+    return TestCase(complex_type=t.NewType('name', int), expected_fact_type=int)
+
+
+@pytest.fixture(scope='session')
+def test_case_Annotated() -> TestCase:
+    return TestCase(complex_type=t.Annotated[int, 'description'], expected_fact_type=int)
+
+
+@pytest.fixture(scope='session')
+def test_case_typing_List() -> TestCase:
+    return TestCase(complex_type=t.List[t.NewType('name', int)], expected_fact_type=t.List[int])    # noqa
+
+
+@pytest.fixture(scope='session')
+def test_case_list() -> TestCase:
+    return TestCase(complex_type=list[t.NewType('name', int)], expected_fact_type=list[int])    # noqa
+
+
+@pytest.fixture(scope='session')
+def test_case_typing_Dict() -> TestCase:
+    return TestCase(
+        complex_type=t.Dict[t.NewType('name', str), t.NewType('name', int)],    # noqa
+        expected_fact_type=t.Dict[str, int],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_dict() -> TestCase:
+    return TestCase(
+        complex_type=dict[t.NewType('name', str), t.NewType('name', int)],    # noqa
+        expected_fact_type=dict[str, int],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_typing_Set() -> TestCase:
+    return TestCase(
+        complex_type=t.Set[t.NewType('name', str)],    # noqa
+        expected_fact_type=t.Set[str],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_set() -> TestCase:
+    return TestCase(
+        complex_type=set[t.NewType('name', str)],    # noqa
+        expected_fact_type=set[str],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_typing_Tuple() -> TestCase:
+    return TestCase(
+        complex_type=t.Tuple[t.NewType('name', str)],    # noqa
+        expected_fact_type=t.Tuple[str],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_tuple() -> TestCase:
+    return TestCase(
+        complex_type=tuple[t.NewType('name', str)],    # noqa
+        expected_fact_type=tuple[str],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_typing_Union() -> TestCase:
+    return TestCase(
+        complex_type=t.Union[
+            t.NewType('name', str),    # noqa
+            t.NewType('name', int),    # noqa
+            t.NewType('name', set),    # noqa
+        ],
+        expected_fact_type=t.Union[str, int, set],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_union1() -> TestCase:
+    return TestCase(
+        complex_type=str | int | set,
+        expected_fact_type=t.Union[str, int, set],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_union2() -> TestCase:
+    complex_type = t.NewType('name', str) | t.NewType('name', int) | t.NewType('name', set)
+    expected_fact_type = t.Union[str, int, set]
+    return TestCase(complex_type=complex_type, expected_fact_type=expected_fact_type)
+
+
+@pytest.fixture(scope='session')
+def test_case_typing_Optional() -> TestCase:
+    return TestCase(
+        complex_type=t.Optional[t.NewType('name', str)],    # noqa
+        expected_fact_type=t.Optional[str],
+    )
+
+
+@pytest.fixture(scope='session')
+def test_case_pydantic_BaseModel() -> TestCase:
+    class MyDTO(DTO):
+        field1: str
+
+    return TestCase(complex_type=MyDTO, expected_fact_type=MyDTO)
+
+
+@pytest.fixture(scope='session')
+def test_case_context() -> TestCase:
+    class MyContext(IContext):
+        field1: str
+
+    return TestCase(complex_type=MyContext, expected_fact_type=MyContext)
+
+
+@pytest.fixture(scope='session')
+def test_case_super_complex_type() -> TestCase:
+    class MyDTO(DTO):
+        field1: str
+
+    # yapf: disable
+    complex_type = t.Dict[
+        t.NewType('name', str), # noqa
+        list[
+            t.Dict[
+                int,
+                t.Tuple[
+                    t.Dict[
+                        t.Set[
+                            t.Dict[
+                                t.NewType(
+                                    'name',  # noqa
+                                    set[t.Annotated[t.NewType('name', str), 'description']], # noqa
+                                ),
+                                float,
+                                ],
+                            ],
+                        set[MyDTO],
+                        ],
+                    ],
+                ],
+            ],
+        ]
+    # yapf: enable
+
+    # yapf: disable
+    expected_fact_type = t.Dict[
+        str,
+        list[
+            t.Dict[
+                int,
+                t.Tuple[
+                    t.Dict[
+                        t.Set[
+                            t.Dict[
+                                set[str],
+                                float,
+                            ],
+                        ],
+                        set[MyDTO],
+                    ],
+                ],
+            ],
+        ],
+    ]
+    # yapf: enable
+
+    return TestCase(complex_type=complex_type, expected_fact_type=expected_fact_type)
+
+
+@pytest.fixture(
+    scope='session',
+    params=[
+        pytest_lazyfixture.lazy_fixture(test_case_fact_types.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_NewType.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_Annotated.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_typing_List.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_list.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_typing_Dict.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_dict.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_typing_Set.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_set.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_typing_Tuple.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_tuple.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_typing_Union.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_union1.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_union2.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_typing_Optional.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_super_complex_type.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_pydantic_BaseModel.__name__),
+        pytest_lazyfixture.lazy_fixture(test_case_context.__name__),
+    ],
+)
+def test_case(request) -> TestCase:
+    return request.param
+
+
+class TestGetFactTypeFromComplexType(TestClass[_get_fact_type_from_complex_type]):
+    def test(self, test_case: TestCase):
+        fact_type = _get_fact_type_from_complex_type(complex_type=test_case.complex_type)
+        assert fact_type == test_case.expected_fact_type
