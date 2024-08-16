23e075e55bf5b1a96eb7f8783d7c2daa405f9e67
Rwwwrl <forgithublesh@yandex.ru>
2024-08-16 14:10:50 +0300

    [ESHOP-109] feat * pytests/pytst_cqrs_contract_usage.py убрал ручное навешивание @pytest.mark.cqrs_contract_usage

--- eshop/framework/framework/pytests/pytest_cqrs_contract_usage.py
+++ eshop/framework/framework/pytests/pytest_cqrs_contract_usage.py
@@ -1,11 +1,8 @@
-import pytest
-
 from framework.for_pytests.for_testing_cqrs_contract_usage import ITestQueryContract, assert_attribute
 
 from user_identity_cqrs_contract.query.query import UserFromJWTTokenQuery, UserIdWithRoleDTO
 
 
-@pytest.mark.cqrs_contract_usage
 class TestUserIdFromJWTTokenQuery(ITestQueryContract[UserFromJWTTokenQuery]):
     def test_query_contract(self):
         assert_attribute(UserFromJWTTokenQuery, 'jwt_token', str)
