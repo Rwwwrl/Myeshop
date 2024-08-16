81596b538a8bb84d703e855ce0270254e4d196eb
Rwwwrl <forgithublesh@yandex.ru>
2024-08-16 13:08:30 +0300

    [ESHOP-110] chore * pytests/pytest_cqrs_contract_usage.py
    - Поправил везде контракты, теперь я указываю fact_expected_type, а не
    complex_type

--- eshop/framework/framework/pytests/pytest_cqrs_contract_usage.py
+++ eshop/framework/framework/pytests/pytest_cqrs_contract_usage.py
@@ -1,22 +1,17 @@
 import pytest
 
-from pytest_check import check
-
 from framework.for_pytests.for_testing_cqrs_contract_usage import ITestQueryContract, assert_attribute
 
-from user_identity_cqrs_contract.hints import JWTToken, UserId
 from user_identity_cqrs_contract.query.query import UserFromJWTTokenQuery, UserIdWithRoleDTO
 
 
 @pytest.mark.cqrs_contract_usage
 class TestUserIdFromJWTTokenQuery(ITestQueryContract[UserFromJWTTokenQuery]):
     def test_query_contract(self):
-        with check:
-            assert_attribute(UserFromJWTTokenQuery, 'jwt_token', JWTToken)
+        assert_attribute(UserFromJWTTokenQuery, 'jwt_token', str)
 
     def test_query_response_contract(self):
         response_type = UserFromJWTTokenQuery.__response_type__()
 
-        with check:
-            assert response_type == UserIdWithRoleDTO
-            assert_attribute(UserIdWithRoleDTO, 'id', UserId)
+        assert response_type == UserIdWithRoleDTO
+        assert_attribute(UserIdWithRoleDTO, 'id', int)
