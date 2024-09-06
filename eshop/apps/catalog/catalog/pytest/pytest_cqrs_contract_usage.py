64a6f44e18b6aca07f88b03ea32b66bd3a4661d3
Rwwwrl <forgithublesh@yandex.ru>
2024-09-06 15:27:56 +0400

    [ESHOP-122] tests catalog pytest_cqrs_contract_usage.py

--- eshop/apps/catalog/catalog/pytest/pytest_cqrs_contract_usage.py
+++ eshop/apps/catalog/catalog/pytest/pytest_cqrs_contract_usage.py
@@ -1,18 +1,20 @@
 from catalog_cqrs_contract.event import (
     CatalogItemHasBeenDeletedEvent,
-    CatalogItemPriceChangedEvent,
+    CatalogItemPriceOrDiscountWasChangedEvent,
 )
 
 from framework.cqrs.context import InsideSqlachemyTransactionContext
 from framework.for_pytests.for_testing_cqrs_contract_usage import ITestEventContractPublisher, assert_attribute
 
 
-class TestCatalogItemPriceChangedEvent(ITestEventContractPublisher[CatalogItemPriceChangedEvent]):
+class TestCatalogItemPriceOrDiscountWasChangedEvent(
+    ITestEventContractPublisher[CatalogItemPriceOrDiscountWasChangedEvent],
+):
     def test_event_contract(self) -> None:
-        assert_attribute(CatalogItemPriceChangedEvent, 'catalog_item_id', int)
-        assert_attribute(CatalogItemPriceChangedEvent, 'old_price', float)
-        assert_attribute(CatalogItemPriceChangedEvent, 'new_price', float)
-        assert_attribute(CatalogItemPriceChangedEvent, 'context', InsideSqlachemyTransactionContext)
+        assert_attribute(CatalogItemPriceOrDiscountWasChangedEvent, 'catalog_item_id', int)
+        assert_attribute(CatalogItemPriceOrDiscountWasChangedEvent, 'new_price', float)
+        assert_attribute(CatalogItemPriceOrDiscountWasChangedEvent, 'new_discount', int)
+        assert_attribute(CatalogItemPriceOrDiscountWasChangedEvent, 'context', InsideSqlachemyTransactionContext)
 
 
 class TestCatalogItemHasBeenDeleted(ITestEventContractPublisher[CatalogItemHasBeenDeletedEvent]):
